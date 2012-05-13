6a22d8cbc0ce97a1721b1079247ce85ae9a2a8a4
Sagar Chalise <chalisesagar@gmail.com>
2012-05-13 12:44:21 +0545

    Changed from applet to indicator and added reference to nepdate converter.

--- install.sh
+++ install.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 
-apt-get install python-gnomeapplet
+apt-get install python-appindicator
 
-cp nepcalapplet.py /usr/local/bin
-chmod a+x /usr/local/bin/nepcalapplet.py
-
-cp nepcalapplet.server /usr/lib/bonobo/servers/
-cp nepalflag.png /usr/share/icons/
+cp nepcalindicator /usr/local/bin
+chmod a+x /usr/local/bin/nepcalindicator
